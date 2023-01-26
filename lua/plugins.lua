734866afceacdfab7cb3fd770019847696f15650
Robert Garbanati <robert.garbanati@automotivepower.com>
2023-01-26 15:20:28 -0800

    dracula seems to work with packer

---
+++ lua/plugins.lua
@@ -0,0 +1,12 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+-- vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- You can alias plugin names
+  use {'Mofiqul/dracula.nvim', as = 'dracula'}
+end)
