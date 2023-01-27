b1e57a146bf1ddc70d5a48dfcc4b0087667e30ba
Robert Garbanati <robert.garbanati@automotivepower.com>
2023-01-26 16:11:50 -0800

    onedark, cmp is working

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,11 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
--- vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- You can alias plugin names
   use {'Mofiqul/dracula.nvim', as = 'dracula'}
+  use 'navarasu/onedark.nvim'
 
   -- Autocompletion plugin
   use {
